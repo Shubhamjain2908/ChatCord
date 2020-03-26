afcb8596b098a7645220cc7dae75dd62cd3c0c99
Shubham Jain <shubhamjain2908@gmail.com>
2020-03-27 02:09:27 +0530

    setting time & text as object

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
 
 const app = express();
 const server = http.createServer(app);
@@ -10,22 +11,24 @@ const io = socketio(server);
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+const botName = 'ChatCord Bot';
+
 // Run when a client connects
 io.on('connection', socket => {
 
     // Welcome current user
-    socket.emit('message', 'Welcome to ChatCord!'); // it will emit to single connected user
+    socket.emit('message', formatMessage(botName, 'Welcome to ChatCord!')); // it will emit to single connected user
 
     // Broadcast when a user connects
-    socket.broadcast.emit('message', 'A user has joined a chat'); // it will emit to every body except the connected user
+    socket.broadcast.emit('message', formatMessage(botName, 'A user has joined a chat')); // it will emit to every body except the connected user
 
     socket.on('disconnect', () => {
-        io.emit('message', 'A user has left the chat'); // it will emit to every user
+        io.emit('message', formatMessage(botName, 'A user has left the chat')); // it will emit to every user
     });
 
     // Listen for chatMessage
     socket.on('chatMessage', msg => {
-        io.emit('message', msg);
+        io.emit('message', formatMessage('USER', msg));
     })
 
 });
