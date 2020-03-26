43dba9215a62881075f8c0ec129c9b50b616f33d
Shubham Jain <shubhamjain2908@gmail.com>
2020-03-27 01:30:06 +0530

    add socket & listem for new connection & welcome msg

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Run when a client connects
+io.on('connection', socket => {
+    console.log('New WS connection...');
+
+    socket.emit('message', 'Welcome to ChatCord!');
+});
+
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
