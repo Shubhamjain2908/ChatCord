ce9d5215218840c30caa79b0b0a71b3bf25616a4
Shubham Jain <shubhamjain2908@gmail.com>
2020-03-30 01:53:22 +0530

    joinroom functionality on server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 const formatMessage = require('./utils/messages');
+const { userJoin, getCurrentUser } = require('./utils/users');
 
 const app = express();
 const server = http.createServer(app);
@@ -16,21 +17,34 @@ const botName = 'ChatCord Bot';
 // Run when a client connects
 io.on('connection', socket => {
 
-    // Welcome current user
-    socket.emit('message', formatMessage(botName, 'Welcome to ChatCord!')); // it will emit to single connected user
+    // Joinroom
+    socket.on('joinRoom', ({ username, room }) => {
+        const user = userJoin(socket.id, username, room);
 
-    // Broadcast when a user connects
-    socket.broadcast.emit('message', formatMessage(botName, 'A user has joined a chat')); // it will emit to every body except the connected user
+        socket.join(user.room);
 
-    socket.on('disconnect', () => {
-        io.emit('message', formatMessage(botName, 'A user has left the chat')); // it will emit to every user
+        // Welcome current user
+        socket.emit('message', formatMessage(botName, 'Welcome to ChatCord!')); // it will emit to single connected user
+
+        // Broadcast when a user connects
+        socket.broadcast
+            .to(user.room)
+            .emit(
+                'message',
+                formatMessage(botName, `${user.username} has joined a chat`)); // it will emit to every body except the connected user
+
     });
 
     // Listen for chatMessage
     socket.on('chatMessage', msg => {
-        io.emit('message', formatMessage('USER', msg));
-    })
+        const user = getCurrentUser(socket.id);
 
+        io.to(user.room).emit('message', formatMessage(user.username, msg));
+    });
+
+    socket.on('disconnect', () => {
+        io.emit('message', formatMessage(botName, 'A user has left the chat')); // it will emit to every user
+    });
 });
 
 const PORT = 3000 || process.env.PORT;
